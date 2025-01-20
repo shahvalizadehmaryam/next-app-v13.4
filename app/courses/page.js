f24d0f126644b2c761a0e379e222525773b423a1
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-01-20 13:06:22 +0330

    clientside page added.

---
+++ app/courses/page.js
@@ -0,0 +1,11 @@
+"use client";
+import { useEffect } from "react";
+
+function Courses() {
+  useEffect(() => {
+    console.log("botostart");
+  }, []);
+  return <div>Courses</div>;
+}
+
+export default Courses;
